5928724ac1bab1b14b5aef2ceb22ba6427c0d54c
IbrahemAtef <bulbul.20001@gmail.com>
2020-07-22 15:54:19 +0300

    front-end v2.2

--- client/src/components/profile.js
+++ client/src/components/profile.js
@@ -5,19 +5,21 @@ class Profile extends React.Component {
     user: null,
   };
 
-  // componentDidMount() {
-  //     if (this.props.location.state === undefined) {
-  //       this.props.history.push('/');
-  //       return;
-  //     }
+  componentDidMount() {
+    if (this.props.location.state === undefined) {
+      this.props.history.push('/');
+      return;
+    }
 
-  //     const { user } = this.props.location.state;
-  //     this.setState({
-  //       user,
-  //     });
-  //   }
+    const { user } = this.props.location.state;
+    console.log(user);
+    this.setState({
+      user: user[0],
+    });
+  }
 
   render() {
+    console.log(this.state.user);
     return (
       <div className='profile'>
         <div className='overlay'>
@@ -41,12 +43,27 @@ class Profile extends React.Component {
               <div className='info'>
                 <ul>
                   <h2>User's Info</h2>
-
-                  <li>Fisrt Name: Ahmed</li>
-                  <li>Last Name: Abd</li>
-                  <li>Age: 33</li>
-                  <li>Balance: 2510$</li>
-                  <li>Joining Date: -</li>
+                  <li>
+                    First Name:
+                    {this.state.user !== null ? this.state.user.firstname : ''}
+                  </li>
+                  <li>
+                    Last Name:{' '}
+                    {this.state.user !== null ? this.state.user.lastname : ''}
+                  </li>
+                  <li>
+                    Age: {this.state.user !== null ? this.state.user.age : ''}
+                  </li>
+                  <li>
+                    Balance:{' '}
+                    {this.state.user !== null ? this.state.user.total : ''}$
+                  </li>
+                  <li>
+                    Joining Date:{' '}
+                    {this.state.user !== null
+                      ? this.state.user.signin.substring(0, 10)
+                      : ''}
+                  </li>
                 </ul>
               </div>
             </div>
