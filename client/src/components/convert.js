8a4d032d3a7cc769a102a75b9f20bf534d727cbe
IbrahemAtef <bulbul.20001@gmail.com>
2020-07-22 14:31:17 +0300

    Front-end v2.1

--- client/src/components/convert.js
+++ client/src/components/convert.js
@@ -1,44 +1,38 @@
 import React from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 
 // this is the deposite component.
 class Convert extends React.Component {
-    state = {}
+  state = {};
 
+  render() {
+    return (
+      <div className='convert'>
+        <div className='overlay'>
+          <div>
+            <form>
+              <select className='ct'>
+                <option>Choose...</option>
+                <option>USD (US$)</option>
+                <option>EUR (â‚¬)</option>
+                <option>GBP (Â£)</option>
+              </select>
 
-    render() {
+              <label>To</label>
 
-        return (
-
-            <div className="convert">
-                <div className="overlay">
-                    <div>
-                        <form>
-                            <select className="ct">
-                                <option>Choose...</option>
-                                <option>USD (US$)</option>
-                                <option>EUR (â‚¬)</option>
-                                <option>GBP (Â£)</option>
-                            </select>
-
-                            <label>To</label>
-
-                            <select className="ct">
-                                <option>Choose...</option>
-                                <option>USD (US$)</option>
-                                <option>EUR (â‚¬)</option>
-                                <option>GBP (Â£)</option>
-                            </select>
-                            <button className='btn'>
-                                Convert
-                            </button>
-                        </form>
-                    </div>
-
-                </div>
-            </div>
-        );
-    }
+              <select className='ct'>
+                <option>Choose...</option>
+                <option>USD (US$)</option>
+                <option>EUR (â‚¬)</option>
+                <option>GBP (Â£)</option>
+              </select>
+              <button className='btn'>Convert</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Convert;
