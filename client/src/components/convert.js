d067e9f86fff1923da6c3bbaf54aa583b96a7a8f
IbrahemAtef <bulbul.20001@gmail.com>
2020-07-23 11:12:20 +0300

    convert done

--- client/src/components/convert.js
+++ client/src/components/convert.js
@@ -1,35 +1,91 @@
 import React from 'react';
-// import axios from 'axios';
+import axios from 'axios';
+import $ from 'jquery';
 
 // this is the deposite component.
 class Convert extends React.Component {
-  state = {};
+  state = {
+    amount: '',
+    from: '',
+    to: '',
+    allCur: [],
+  };
+
+  componentDidMount() {
+    axios
+      .get('/getAllCur')
+      .then((response) => {
+        var x = Object.keys(response.data);
+        var y = [];
+        for (var i in x) {
+          y.push(x[i].substring(3));
+        }
+        this.setState({
+          allCur: y,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const { amount, from, to } = this.state;
+    axios
+      .post('/convert', { amount, from, to })
+      .then((response) => {
+        $('.converter').text(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 
   render() {
     return (
       <div className='convert'>
         <div className='overlay'>
           <div>
-            <form>
-              <input type='text' placeholder='Enter Amount' />
-              <select className='ct'>
+            <form onSubmit={this.handleSubmit.bind(this)}>
+              <input
+                type='text'
+                placeholder='Enter Amount'
+                name='amount'
+                onChange={this.handleChange.bind(this)}
+              />
+              <select
+                className='ct'
+                onChange={this.handleChange.bind(this)}
+                name='from'
+              >
                 <option>Choose...</option>
-                <option>USD (US$)</option>
-                <option>EUR (â‚¬)</option>
-                <option>GBP (Â£)</option>
+                {this.state.allCur.map((e, i) => {
+                  return <option key={i}>{e}</option>;
+                })}
               </select>
 
               <p>To</p>
 
-              <select className='ct'>
+              <select
+                className='ct'
+                onChange={this.handleChange.bind(this)}
+                name='to'
+              >
                 <option>Choose...</option>
-                <option>USD (US$)</option>
-                <option>EUR (â‚¬)</option>
-                <option>GBP (Â£)</option>
+                {this.state.allCur.map((e, i) => {
+                  return <option key={i}>{e}</option>;
+                })}
               </select>
               <button className='btn'>Convert</button>
             </form>
-            <p className='converter'>154.515$</p>
+            <p className='converter'>0</p>
           </div>
         </div>
       </div>
